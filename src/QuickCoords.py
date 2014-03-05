63d287306fc02d2307cae914850c07fc9edc5b5b
AlphaSheep <brendan@alphasheep.co.za>
2014-03-05 17:24:35 +0200

    Added basic image loader.

--- src/QuickCoords.py
+++ src/QuickCoords.py
@@ -9,6 +9,10 @@ import sys, os#, shutil
 from PyQt4.QtCore import Qt
 from PyQt4 import QtGui
 
+
+supportedExtensions = ['png', 'jpg', 'jpeg', 'bmp', 'gif']
+
+
 class ToolScreen(QtGui.QWidget):
     
     def __init__(self):
@@ -25,18 +29,23 @@ class ToolScreen(QtGui.QWidget):
         
         self.imagePath = ""
 
-        self.imageBlock = QtGui.QLabel(self) # Just a dummy label to hold an image
-        self.imageLabel = QtGui.QLabel(self.imagePath, self)
+        self.imagePathLabel = QtGui.QLabel("", self)
+        self.imageLabel = QtGui.QLabel("No image loaded.", self)
+        self.image = QtGui.QPixmap()
+        self.imageBlock = QtGui.QLabel(self) # Just a dummy label to hold the image
+        self.imageBlock.setPixmap(self.image)
+        
         folderButton = QtGui.QPushButton("Image folder:")
         folderButton.clicked.connect(self.selectFolder)
         
         editBlock = QtGui.QTextEdit()
        
         titleBox.addWidget(folderButton)
-        titleBox.addWidget(self.imageLabel)
+        titleBox.addWidget(self.imagePathLabel)
         imageBox.addLayout(titleBox)
         imageBox.addWidget(self.imageBlock)
         imageBox.addStretch(1)
+        imageBox.addWidget(self.imageLabel)
         
         outputBox.addWidget(editBlock)
         
@@ -54,9 +63,27 @@ class ToolScreen(QtGui.QWidget):
 
     def selectFolder(self):
         
-        self.imagePath = QtGui.QFileDialog.getExistingDirectory(self, "Select a folder with images")
-        self.imageLabel.setText(self.imagePath)
+        self.imagePath = QtGui.QFileDialog.getExistingDirectory(self, "Select a folder with images", self.imagePath)+'/'
+        self.imagePathLabel.setText(self.imagePath)
+        fileList = os.listdir(self.imagePath)
+        self.imageList = []
+        for i in fileList:
+            extension = i.split('.')[-1]
+            if extension in supportedExtensions:
+                self.imageList.append(self.imagePath + i)
+                self.imageList[-1].replace('\\','/') # Replace Windows' stupid file seperator with one that works on all platforms.
+        self.currentImageNum = 0
+        print (self.imageList)
         
+        self.setImage()
+        
+
+    def setImage(self):
+        currentImage = self.imageList[self.currentImageNum]
+        print("Attempting to load Current image",currentImage)
+        self.image = QtGui.QPixmap(currentImage)
+        self.imageBlock.setPixmap(self.image)
+        self.imageLabel.setText(currentImage.split('/')[-1])
         
     
         
