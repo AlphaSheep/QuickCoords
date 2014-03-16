b5a933fb9ee57788c47ae652d71e3bcad0d60d41
AlphaSheep <brendan@alphasheep.co.za>
2014-03-16 11:51:36 +0200

    General code cleanup and documentation added.

--- src/QuickCoords.py
+++ src/QuickCoords.py
@@ -62,6 +62,10 @@ targetFPS = 30
 #===================#
 
 class Point():
+    '''
+    Provides a basic point functionality with x, y coordinates.
+        Point.shift(direction, amount) method shifts a point by a certain amount in a specified direction.
+    '''
     
     def __init__(self, x, y):
         
@@ -71,6 +75,10 @@ class Point():
         
     
     def shift(self, direction, amount):
+        '''
+        Shifts the point by amount in the specified direction.
+        Direction can be 'up', 'down', 'left' or 'right'.
+        '''
         
         if direction == 'up':
             self.y -= amount
@@ -86,13 +94,25 @@ class Point():
             self.y = round(self.y)
             
         
-        
     def __str__(self):
         
         return '('+str(self.x)+', '+str(self.y)+')'
 
 
 class CoordinateList():
+    '''
+    Provides functionality for dealing with a list of points.
+    Provides the following methods:
+        CoordinateList.addPoint(point) adds a point to the list.
+        CoordinateList.addPoints(points) adds a list of points to the list.
+        CoordinateList.removeLastPoint() removes the last point from the list.
+        CoordinateList.clear() removes all points.
+        CoordinateList.length() returns the length of the coordinate list.
+        CoordinateList.removePoint(n) removes the point with index n.
+        CoordinateList.getPointIndex(point) returns the index of a point close to the specified point.
+        CoordinateList.copyAsText() returns a tab separated string of points.
+        CoordinateList.copyAsCSV() returns a comma separated string of points.
+    '''
     
     def __init__(self, initPoints):
         
@@ -100,32 +120,50 @@ class CoordinateList():
 
 
     def addPoint(self, point):
+        '''
+        Adds a point to the coordinate list.
+        '''
         
         self.points.append(point)
         
     
     def addPoints(self, points):
+        '''
+        Adds all points in the list of points to the coordinate list. 
+        '''
         
         for p in points:
             self.points.append(p)
             
             
     def removeLastPoint(self):
+        '''
+        Removes the last point from the list.
+        '''
         
         self.points = self.points[:-1]
     
     
     def clear(self):
+        '''
+        Removes all points from the list.
+        '''
         
         self.points = []
         
          
     def length(self):
+        '''
+        Returns the number of points in the list
+        '''
         
         return len(self.points)
     
     
     def removePoint(self, n):
+        '''
+        Removes the nth point from the list.
+        '''
         
         if n >= len(self.points):
             self.points[n] # Lazily force list index out of range, otherwise it would have passed silently.
@@ -134,6 +172,10 @@ class CoordinateList():
         
     
     def getPointIndex(self, point):
+        '''
+        Finds a point in the list within selectionRadius of the specified point.
+        Requires the constant selectionRadius to be defined within the current namespace.
+        '''
         
         for i in range(self.length()):
             p = self.points[i]
@@ -143,6 +185,10 @@ class CoordinateList():
         
     
     def copyAsText(self):
+        '''
+        Returns a tab separated string of points suitable for copying into spreadsheet programs such as 
+        Microsoft Excel or LibreOffice Calc.  
+        '''
         
         res = ''
         for p in self.points:
@@ -151,6 +197,9 @@ class CoordinateList():
 
 
     def copyAsCSV(self):
+        '''
+        Returns a comma separated string of points suitable for writing into a CSV file. 
+        '''
         
         res = ''
         for p in self.points:
@@ -167,39 +216,69 @@ class CoordinateList():
 
 
 class ClickableImageBox(QtGui.QGraphicsScene):
+    '''
+    Extends the QGraphicsScene to provide additional functionality.
+    Provides the following function:
+        ClickableImageBox.mouseReleaseEvent(event, *args, **kwargs) handles clicking to add and select points on the image.
+    '''
     
     def mouseReleaseEvent(self, *args, **kwargs):
+        '''
+        Handles left clicking to add and select points on the image and right clicking to remove the last point,
+        then calls the mouseReleaseEvent() method of the QGraphicsScene.
+        '''        
         
         event = args[0]
         if event.button() == Qt.LeftButton:
             point = Point(event.scenePos().x()/imageScaleFactor, event.scenePos().y()/imageScaleFactor)
             nearPoint = self.parent().coordList.getPointIndex(point)
             if nearPoint >= 0:
+                # The point is an existing point
                 selectedPoints = self.parent().table.getSelectedPoints()
                 if not nearPoint in selectedPoints:
+                    # The point exists, but is not yet selected.
                     if event.modifiers() & (Qt.ShiftModifier | Qt.ControlModifier):
+                        # If Shift or Ctrl are being pressed, add the point to the current selection.
                         selectedPoints.append(nearPoint)
                     else:
+                        # Otherwise, the point becomes the current selection.
                         selectedPoints = [nearPoint]
                 else:
+                    # The point already exists, and is already selected.
                     if event.modifiers() & Qt.ControlModifier:
+                        # If Ctrl is being pressed, remove the point from the selection.
                         selectedPoints.remove(nearPoint)
-                    elif not event.modifiers() & Qt.ShiftModifier:
+                    elif not (event.modifiers() & Qt.ShiftModifier):
+                        # Otherwise, deselect all other points. 
                         selectedPoints = [nearPoint]
                 self.parent().table.setSelectedRows(selectedPoints)
                 
             else:
                 self.parent().coordList.addPoint(point)
+                
         if event.button() == Qt.RightButton:
             self.parent().coordList.removeLastPoint()
+            
         self.parent().tableViewChanged = True
 
         return QtGui.QGraphicsScene.mouseReleaseEvent(self, *args, **kwargs)
 
 
 class TableBox(QtGui.QTableWidget):
+    '''
+    Extends QTableWidget to provide the following functions:
+        TableBox.keyPressEvent(event, *args, **kwargs) Unnecessary function. To be removed.
+        TableBox.deleteSelectedRows() deletes all points that are currently selected.
+        TableBox.selectionChanged(*args, **kwargs) changes the colours of points depending on whether or not they are selected.
+        TableBox.getSelectedPoints() returns a list of the currently selected points.
+        TableBox.setSelectedRows(rows) Selects all points in the list of indices.
+        
+    '''
     
     def keyPressEvent(self, *args, **kwargs):
+        '''
+        Unnecessary function. To be removed.
+        '''
         
         event = args[0]
         self.toolScreen.ignoreDeletes = True
@@ -208,6 +287,9 @@ class TableBox(QtGui.QTableWidget):
     
     
     def deleteSelectedRows(self):
+        '''
+        Deletes all rows that are currently selected and updates the parent's coordinate list.
+        '''
         
         newCoordList = CoordinateList([])
         oldCoordList = self.toolScreen.coordList
@@ -222,6 +304,9 @@ class TableBox(QtGui.QTableWidget):
         
     
     def selectionChanged(self, *args, **kwargs):
+        '''
+        Updates the point colours if they are selected.
+        '''
         
         selectedPoints = self.getSelectedPoints()
         
@@ -236,6 +321,9 @@ class TableBox(QtGui.QTableWidget):
     
     
     def getSelectedPoints(self):
+        '''
+        Returns a list of the currently selected points.  
+        '''
         
         selectedPoints = []
         for index in self.selectionModel().selectedRows():
@@ -244,6 +332,9 @@ class TableBox(QtGui.QTableWidget):
             
         
     def setSelectedRows(self, rows):
+        '''
+        Selects all points specified in rows, which is a list of indices to be selected.
+        '''
         
         self.clearSelection()
         selectedItems = self.selectionModel().selection()
@@ -255,6 +346,30 @@ class TableBox(QtGui.QTableWidget):
 
 
 class ToolScreen(QtGui.QWidget):
+    '''
+    Extends QWidget to provide the required functionality for the program.
+    Provides the following functions:
+        ToolScreen.prepare() initialises some variables.
+        ToolScreen.updateDisplay() fills the table and redraws the points.
+        ToolScreen.keyPressEvent(event) handles keyboard shortcuts.
+        ToolScreen.initUI() initialises the user interface.
+        ToolScreen.selectFolder() brings up a folder selection dialogue.
+        ToolScreen.setFoldertoPath(newPath) changes the current folder.
+        ToolScreen.copyTable() copies the list of points to the clipboard.
+        ToolScreen.exportTable() exports the list of points to a CSV or plain text file.
+        ToolScreen.clearTable() deletes all points.
+        ToolScreen.fillListBox() fills the list box with the images from the current folder.
+        ToolScreen.changeImageFromList() changes the image to the currently selected image in the list box.
+        ToolScreen.shiftSelected(direction) shifts the selected points in the specified direction.
+        ToolScreen.setImage() loads the current image from disk and sets it for display.
+        ToolScreen.updatePoints() updates the table to reflect the current state of the coordinate list.
+        ToolScreen.drawImagePoints() redraws the points on the display.
+        ToolScreen.nextImage() switches to the next image.
+        ToolScreen.prevImage() switches to the previous image.
+        ToolScreen.saveCurrentFolder() writes the current path to a file.
+        ToolScreen.loadLastFolder() loads the folder last used.
+    '''
+    
     
     def __init__(self):
 
@@ -269,6 +384,9 @@ class ToolScreen(QtGui.QWidget):
 
 
     def prepare(self):
+        '''
+        Initialises variables that need to be set before the GUI is initialised.
+        '''
 
         self.loadLastFolder()
         self.currentImageNum = 0
@@ -280,6 +398,9 @@ class ToolScreen(QtGui.QWidget):
                
                      
     def updateDisplay(self):
+        '''
+        Fills the table and redraws the points, if things have changed since the last update.
+        '''
         
         if self.tableViewChanged:
             self.updatePoints()
@@ -288,6 +409,9 @@ class ToolScreen(QtGui.QWidget):
                 
     
     def keyPressEvent(self, event):
+        '''
+        Handles key presses anywhere in the program.
+        '''
         
         if event.key() in forwardKeys:
             self.nextImage()
@@ -311,6 +435,9 @@ class ToolScreen(QtGui.QWidget):
 
         
     def initUI(self):
+        '''
+        Initialises the UI layout and widgets.
+        '''
         
         mainBox = QtGui.QHBoxLayout()
         
@@ -344,9 +471,7 @@ class ToolScreen(QtGui.QWidget):
         tableClearButton = QtGui.QPushButton("Clear")
         tableClearButton.setMinimumWidth(40)
         tableClearButton.clicked.connect(self.clearTable)
-
         
-        #self.editBlock = QtGui.QTextEdit()
         self.table = TableBox(0,2)
         self.table.toolScreen = self
         self.table.setHorizontalHeaderLabels(['x','y'])
@@ -408,6 +533,9 @@ class ToolScreen(QtGui.QWidget):
 
 
     def selectFolder(self):
+        '''
+        Brings up a folder selection dialog and sets the current path to the selcted folder.
+        '''
         
         fileDialog = QtGui.QFileDialog()
         newPath = fileDialog.getExistingDirectory(self, "Select a folder with images", self.imagePath)
@@ -415,9 +543,12 @@ class ToolScreen(QtGui.QWidget):
 
         
     def setFoldertoPath(self, newPath):
+        '''
+        Handles a change in path. Checks that the path exists, fills the list box, loads the first image, and saves the current folder.
+        '''
         
         if len(newPath) > 0 and os.access(newPath, 0):
-            self.imagePath = newPath.replace('\\','/').rstrip('/')+'/' # Replace Windows' stupid file seperator with one that works on all platforms.
+            self.imagePath = newPath.replace('\\','/').rstrip('/')+'/' # Replace Windows' stupid file separator with one that works on all platforms.
             self.imagePathLabel.setText(self.imagePath)
             fileList = os.listdir(self.imagePath)
             fileList.sort()
@@ -434,11 +565,17 @@ class ToolScreen(QtGui.QWidget):
         
     
     def copyTable(self):
+        '''
+        Copies a tab separated list to the clipboard, suitable for pasting into most spreadsheet programs
+        '''
         
         self.clipboard.setText(self.coordList.copyAsText())
 
 
     def exportTable(self):
+        '''
+        Saves a CSV or plain text file containg a comma separated list of points.
+        '''
         
         fileDialog = QtGui.QFileDialog()
         filters = 'CSV files (*.csv);;Text files (*.txt);;All files (*.*)'
@@ -448,20 +585,21 @@ class ToolScreen(QtGui.QWidget):
         exportFile.write(self.coordList.copyAsCSV())
         exportFile.close()
         
-        
-    def saveData(self):
-        
-        pass
-         
-
 
     def clearTable(self):
+        '''
+        Deletes all points.
+        '''
         
         self.coordList.clear()
         self.tableViewChanged = True
 
     
     def fillListBox(self):
+        '''
+        Fills the list box with the names of the images in the folder.
+        Note: Does not check if images have been added since the folder was loaded. To do this, reset the folder.
+        '''
         
         self.listBlock.clear()
         if len(self.imageList) > 0:
@@ -475,12 +613,19 @@ class ToolScreen(QtGui.QWidget):
 
         
     def changeImageFromList(self):
+        '''
+        Changes the image to the currently selected image in the list.
+        '''
         
         self.currentImageNum = self.listBlock.currentRow()
         self.setImage()
     
 
     def shiftSelected(self, direction):
+        '''
+        Moves the selected points 1 pixel (on the screen, not on the image) in the specified direction.
+        Direction can be 'up', 'down', 'left' or 'right'.
+        '''
         
         selectedPoints = self.table.getSelectedPoints()
         for i in selectedPoints:
@@ -488,6 +633,9 @@ class ToolScreen(QtGui.QWidget):
         
 
     def setImage(self):
+        '''
+        Loads the current image from disk and sets it for display.
+        '''
         
         if len(self.imageList) > 0:
             currentImage = self.imageList[self.currentImageNum]
@@ -504,12 +652,14 @@ class ToolScreen(QtGui.QWidget):
             self.imageBlockScene.addPixmap(self.image)
             self.imageLabel.setText(currentImage.split('/')[-1])
             self.listBlock.setCurrentRow(self.currentImageNum)
-            #self.updatePoints()
         else:
             print("No images in current folder")
         
         
     def updatePoints(self):
+        '''
+        Updates the table to reflect the current state of the coordinate list.
+        '''
 
         selectedPoints = self.table.getSelectedPoints()
         points = self.coordList.points
@@ -527,7 +677,13 @@ class ToolScreen(QtGui.QWidget):
         
             
     def drawImagePoints(self):
+        '''
+        Redraws the points on the display.
+        '''
         
+        # We don't want to actually draw the points onto the image, because then we'd have to reload the
+        # image from disk to undo the changes. Instead, we work on a copy, and only display the copy.
+        # The original image remains unchanged in memory.
         newImage = self.originalImage.toImage()
         
         for p in self.coordList.points:
@@ -555,6 +711,9 @@ class ToolScreen(QtGui.QWidget):
  
     
     def nextImage(self):
+        '''
+        Changes to the next image.
+        '''
         
         self.currentImageNum += 1
         if self.currentImageNum >= len(self.imageList):
@@ -563,6 +722,9 @@ class ToolScreen(QtGui.QWidget):
             
                    
     def prevImage(self):
+        '''
+        Changes to the previous image.
+        '''
         
         self.currentImageNum -= 1
         if self.currentImageNum < 0:
@@ -571,6 +733,9 @@ class ToolScreen(QtGui.QWidget):
             
                 
     def saveCurrentFolder(self):
+        '''
+        Saves the current folder to a file, allowing the folder selection to be persistant if the program is exited.
+        '''
         
         try:
             folderFile = open(folderSaveFileName, 'w')
@@ -581,6 +746,9 @@ class ToolScreen(QtGui.QWidget):
             
         
     def loadLastFolder(self):
+        '''
+        Loads the last current folder from disk.
+        '''
         
         try:
             folderFile = open(folderSaveFileName, 'r')
@@ -592,8 +760,7 @@ class ToolScreen(QtGui.QWidget):
             print("Could not load last folder")
             self.imagePath = ""
         
-                              
-        
+                                      
 def main():
     
     app = QtGui.QApplication(sys.argv)
